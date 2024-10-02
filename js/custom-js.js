b4efe11bb9e23b9101335daf77da27fdf41a2de1
palashroy121 <palash.cmt@gmail.com>
2024-10-03 01:44:47 +0600

    index page section title edit

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,9 +1,12 @@
 // JavaScript to reveal the sticky div on scroll
 window.addEventListener('scroll', function() {
     const stickyDiv = document.getElementById('sticky-div');
+    const stickyLogo = document.getElementById('sticky-logo');
     if (window.scrollY > 200) { // adjust the scroll value as needed
       stickyDiv.style.display = 'none'; // hide the div
+      stickyLogo.style.height = '45px';
     } else {
       stickyDiv.style.display = 'flex'; // show the div
+      stickyLogo.style.height = '64px';
     }
 });
