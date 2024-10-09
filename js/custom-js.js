fe6bb0dfbf0756c3ccc7e0470203d98274f85518
pcroy <pcroy@usb.com>
2024-10-09 17:56:24 +0600

    header fix

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,87 +1,77 @@
-// JavaScript to reveal the sticky div on scroll
-window.addEventListener('scroll', function() {
-    const stickyDiv = document.getElementById('sticky-div');
-    const stickyLogo = document.getElementById('sticky-logo');
+const section = document.querySelector('main');
+const stickyDiv = document.getElementById('sticky-div');
+const stickyLogo = document.getElementById('sticky-logo');
 
-    window.addEventListener('scroll', function() {
-      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-      console.log(scrollTop);
-      if (scrollTop > 180) {
-        stickyDiv.style.display = 'none'; // hide the div
-        stickyLogo.style.height = '45px';
-      } else {
-        stickyDiv.style.display = 'flex'; // show the div
-        stickyLogo.style.height = '48px';
-      }
-  });
+function checkScroll() {
+  const sectionTop = section.getBoundingClientRect().top;
 
-    // console.log(window.scrollY);
-    // if (window.scrollY > 200) { // adjust the scroll value as needed
-    //   stickyDiv.style.display = 'none'; // hide the div
-    //   stickyLogo.style.height = '45px';
-    // } else {
-    //   stickyDiv.style.display = 'flex'; // show the div
-    //   stickyLogo.style.height = '64px';
-    // }
-});
+  if (sectionTop < -200) {
+    stickyDiv.style.display = 'none'; // hide the div
+    stickyLogo.style.height = '45px';
+  } else {
+    stickyDiv.style.display = 'flex'; // show the div
+    stickyLogo.style.height = '48px';
+  }
+}
 
-// Sidebar Menu & Search
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (!isMobile) {
+  window.addEventListener('scroll', checkScroll);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	const body = document.querySelector('body');
-	const menuContainer = document.getElementById('menu-container');
-	const menuButton = document.getElementById('menu-button');
-	const menuCloseButton = document.getElementById('menu-close-button');
+  const body = document.querySelector('body');
+  const menuContainer = document.getElementById('menu-container');
+  const menuButton = document.getElementById('menu-button');
+  const menuCloseButton = document.getElementById('menu-close-button');
 
   const searchContainer = document.getElementById('search-container');
-	const searchButton = document.getElementById('search-button');
-	const searchClose = document.getElementById('search-close');
+  const searchButton = document.getElementById('search-button');
+  const searchClose = document.getElementById('search-close');
 
   const menuToggle = () => {
-		// body.classList.toggle('overflow-hidden');
+    // body.classList.toggle('overflow-hidden');
     menuContainer.classList.toggle('flex');
-		menuContainer.classList.toggle('hidden');
+    menuContainer.classList.toggle('hidden');
   };
 
-	menuButton.addEventListener('click', menuToggle);
-	menuContainer.children[0].addEventListener('click', menuToggle);
-	menuCloseButton.addEventListener('click', menuToggle);
+  menuButton.addEventListener('click', menuToggle);
+  menuContainer.children[0].addEventListener('click', menuToggle);
+  menuCloseButton.addEventListener('click', menuToggle);
 
   const searchToggle = () => {
-		searchContainer.classList.toggle('hidden');
-		searchContainer.classList.toggle('flex');
+    searchContainer.classList.toggle('hidden');
+    searchContainer.classList.toggle('flex');
   };
 
   searchButton.addEventListener('click', searchToggle);
   searchClose.addEventListener('click', searchToggle);
 
-});
+  // Tabs
+  let tabsContainer = document.querySelector("#tabs");
 
+  let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
 
-// Tabs
-let tabsContainer = document.querySelector("#tabs");
+  console.log(tabTogglers);
 
-let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+  tabTogglers.forEach(function (toggler) {
+    toggler.addEventListener("click", function (e) {
+      e.preventDefault();
 
-console.log(tabTogglers);
+      let tabName = this.getAttribute("href");
 
-tabTogglers.forEach(function(toggler) {
-  toggler.addEventListener("click", function(e) {
-    e.preventDefault();
+      let tabContents = document.querySelector("#tab-contents");
 
-    let tabName = this.getAttribute("href");
+      for (let i = 0; i < tabContents.children.length; i++) {
 
-    let tabContents = document.querySelector("#tab-contents");
+        tabTogglers[i].parentElement.classList.remove("border-b-[3px]", "border-[#f48c06]", "dark:border-[#f3722c]", "bg-[#adb5bd]", "dark:bg-[#415a77]"); tabContents.children[i].classList.remove("hidden");
+        if ("#" + tabContents.children[i].id === tabName) {
+          continue;
+        }
+        tabContents.children[i].classList.add("hidden");
 
-    for (let i = 0; i < tabContents.children.length; i++) {
-      
-      tabTogglers[i].parentElement.classList.remove("border-b-[3px]", "border-[#f48c06]", "dark:border-[#f3722c]", "bg-[#adb5bd]", "dark:bg-[#415a77]");  tabContents.children[i].classList.remove("hidden");
-      if ("#" + tabContents.children[i].id === tabName) {
-        continue;
       }
-      tabContents.children[i].classList.add("hidden");
-      
-    }
-    e.target.parentElement.classList.add("border-b-[3px]", "border-[#f48c06]", "dark:border-[#f3722c]", "bg-[#adb5bd]", "dark:bg-[#415a77]");
+      e.target.parentElement.classList.add("border-b-[3px]", "border-[#f48c06]", "dark:border-[#f3722c]", "bg-[#adb5bd]", "dark:bg-[#415a77]");
+    });
   });
 });
-// Tabs End
